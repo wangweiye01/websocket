8105938daf79ab71fc77ab73713fec9f55921e02
wangweiye <wwyknight@163.com>
2019-06-12 09:23:00 +0800

    fix

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -10,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
@@ -20,32 +21,48 @@ import org.springframework.web.bind.annotation.GetMapping;
 @SpringBootApplication
 public class App {
 
-	public static void main(String[] args) {
-		SpringApplication.run(App.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
 
-	@Autowired
-	private SimpMessagingTemplate messagingTemplate;
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
 
-	@GetMapping("/")
-	public String index() {
-		return "index";
-	}
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
 
-	@MessageMapping("/send")
-	@SendTo("/topic/send")
-	public SocketMessage send(SocketMessage message) throws Exception {
-		DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		message.date = df.format(new Date());
-		return message;
-	}
+    /*@MessageMapping("/send")
+    @SendTo("/topic/send")
+    public SocketMessage send(SocketMessage message) throws Exception {
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        message.date = df.format(new Date());
+        return message;
+    }*/
 
-	@Scheduled(fixedRate = 1000)
-	@SendTo("/topic/callback")
-	public Object callback() throws Exception {
-		// 发现消息
-		DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		messagingTemplate.convertAndSend("/topic/callback", df.format(new Date()));
-		return "callback";
-	}
+    @Scheduled(fixedRate = 1000)
+    @SendTo("/topic/callback")
+    public Object callback() throws Exception {
+        // 发现消息
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        messagingTemplate.convertAndSend("/topic/callback", df.format(new Date()));
+        return "callback";
+    }
+
+    @Scheduled(fixedRate = 2000)
+    @SendToUser("/greetings")
+    public String greeting1() {
+        messagingTemplate.convertAndSendToUser("2", "/greetings", "我是卖报的小行家");
+        return "OK";
+    }
+
+    @MessageMapping("/send")
+    @SendToUser("/greetings")
+    public SocketMessage greeting(SocketMessage message) {
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        message.date = df.format(new Date());
+        messagingTemplate.convertAndSendToUser("1", "/greetings", message);
+        return message;
+    }
 }
--- src/main/java/com/example/WebSocketConfig.java
+++ src/main/java/com/example/WebSocketConfig.java
@@ -12,8 +12,9 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry config) {
-		config.enableSimpleBroker("/topic");
+		config.enableSimpleBroker("/topic", "/user");
 		config.setApplicationDestinationPrefixes("/app");
+		config.setUserDestinationPrefix("/user/");
 	}
 
 	@Override
