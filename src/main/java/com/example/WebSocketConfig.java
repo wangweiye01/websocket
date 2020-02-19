e1a969eddd3157de6f8ec05054b1e14cb3423861
wangweiye <wwyknight@163.com>
2020-02-19 13:38:30 +0800

    fix

--- src/main/java/com/example/WebSocketConfig.java
+++ src/main/java/com/example/WebSocketConfig.java
@@ -10,23 +10,25 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 @EnableWebSocketMessageBroker
 public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
-	/**
-	 * 配置消息代理 启动简单Broker，消息的发送的地址符合配置的前缀来的消息才发送到这个broker
-	 * @param config
-	 */
-	@Override
-	public void configureMessageBroker(MessageBrokerRegistry config) {
-		config.enableSimpleBroker("/topic", "/user");
-		config.setApplicationDestinationPrefixes("/app");
-		config.setUserDestinationPrefix("/user/");
-	}
+    /**
+     * 配置消息代理 启动简单Broker，消息的发送的地址符合配置的前缀来的消息才发送到这个broker
+     *
+     * @param config
+     */
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic", "/user");
+        config.setApplicationDestinationPrefixes("/app");
+        config.setUserDestinationPrefix("/user/");
+    }
 
-	/**
-	 * 注册STOMP的端点
-	 * @param registry
-	 */
-	@Override
-	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/my-websocket").withSockJS();
-	}
+    /**
+     * 注册STOMP的端点
+     *
+     * @param registry
+     */
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/my-websocket").withSockJS();
+    }
 }
